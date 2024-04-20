b3db8215f37e4133a4cf47ba77ad4c6d8a9d7e8f
Aireenelz <u2100667@siswa.um.edu.my>
2024-04-20 17:04:07 +0800

    2 column layout

--- module2/scripts/job-search.js
+++ module2/scripts/job-search.js
@@ -1,3 +1,4 @@
+/* Banner dynamic ========================================================================================================= */
 // Initial state
 let jobTitleQuery = "";
 let locationQuery = "";
@@ -6,14 +7,50 @@ let locationQuery = "";
 function handleJobTitleInputChange(event) {
     jobTitleQuery = event.target.value;
     console.log(jobTitleQuery);
+    filterJobs();
 }
 
 function handleLocationInputChange(event) {
     locationQuery = event.target.value;
     console.log(locationQuery);
+    filterLocation();
 }
 
 // Add event listener to input fields
 document.getElementById("job-title").addEventListener("input", handleJobTitleInputChange);
 document.getElementById("job-location").addEventListener("input", handleLocationInputChange);
 
+/* Jobs List dynamic ========================================================================================================= */
+let jobs = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("./dummy-data/jobs.json")
+        .then(res => res.json())
+        .then(data => {
+            jobs = data;
+            console.log(jobs); // Log the fetched JSON data
+        })
+        .catch(error => {
+            console.error("Error fetching jobs:", error);
+        });
+});
+
+// Function to filter jobs based on jobTitleQuery
+function filterJobs() {
+    const filteredJobs = jobs.filter(job => {
+        const jobTitle = job.jobTitle.toLowerCase();
+        return jobTitle.includes(jobTitleQuery.toLowerCase());
+    });
+    
+    console.log(filteredJobs);
+}
+
+// Function to filter jobs based on locationQuery
+function filterLocation() {
+    const filteredLocations = jobs.filter(job => {
+        const location = job.jobLocation.toLowerCase();
+        return location.includes(locationQuery);
+    });
+    
+    console.log(filteredLocations);
+}
