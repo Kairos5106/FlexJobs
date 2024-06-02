c1d96a4621a8aabc9f42a6fe75a7c38adc149976
TheUnknownOriginal <yungzheng90@gmail.com>
2024-06-03 03:11:39 +0800

    updated module4 to payments

--- flexjobs-client/src/Router/Router.jsx
+++ flexjobs-client/src/Router/Router.jsx
@@ -5,7 +5,7 @@ import App from "../App";
 import Home from "../modules/Module1/Home";
 import JobSearch from "../modules/Module2/JobSearch";
 import Module3Page from "../modules/Module3/Module3Page";
-import Module4Page from "../modules/Module4/Module4Page";
+import Payments from "../modules/Module4/Payments";
 import Module5Page from "../modules/Module5/Module5Page";
 import Module6Page from "../modules/Module6/Module6Page";
 
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         { path: "/", element: <Home/> },
         { path: "/JobSearch", element: <JobSearch/> },
         { path: "/Module3Page", element: <Module3Page/> },
-        { path: "/Module4Page", element: <Module4Page/> },
+        { path: "/Payments", element: <Payments/> },
         { path: "/Module5Page", element: <Module5Page/> },
         { path: "/Module6Page", element: <Module6Page/> }
       ]
---
+++ flexjobs-client/src/modules/Module4/Payments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Payments = () => {
+    return (
+        <div className='top-section'>This is Payments Page</div>
+    )
+}
+
+export default Payments
