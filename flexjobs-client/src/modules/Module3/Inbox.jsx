81ea608c94b282fbe8eaec05565deb52f3259d40
dominicchong <rydominicchong@gmail.com>
2024-06-05 15:34:59 +0800

    Changes to Inbox and Feedback

--- flexjobs-client/src/modules/Module3/Inbox.jsx
+++ flexjobs-client/src/modules/Module3/Inbox.jsx
@@ -4,69 +4,69 @@ import './script.js';
 
 const Inbox = () => {
     return (
-        <div class="container-inbox">
-            <div class="column-inbox">
-                <h1 class="h1-inbox">Chats</h1>
-                <div class="row-inbox">
-                    <table class="table-inbox" id="inbox-table">
-                        <tr class="selected-inbox" data-image="./images-inbox/msg1.png">
+        <div className="container-inbox">
+            <div className="column-inbox">
+                <h1 className="h1-inbox">Chats</h1>
+                <div className="row-inbox">
+                    <table className="table-inbox" id="inbox-table">
+                        <tr className="selected-inbox" data-image="./images-inbox/msg1.png">
                             <td>
-                                <p class="name">Mr Ahmad</p>
-                                <p class="message">Hey there! I am...</p>
+                                <p className="name">Mr Ahmad</p>
+                                <p className="message">Hey there! I am...</p>
                             </td>
-                            <td class="timestamp">10:34am</td>
+                            <td className="timestamp">10:34am</td>
                         </tr>
 
                         <tr data-image="./images-inbox/msg2.png">
                             <td>
-                                <p class="name">Martha</p>
-                                <p class="message">I will need someone...</p>
+                                <p className="name">Martha</p>
+                                <p className="message">I will need someone...</p>
                             </td>
-                            <td class="timestamp">3:31pm</td>
+                            <td className="timestamp">3:31pm</td>
                         </tr>
                         <tr data-image="./images-inbox/msg3.png">
                             <td>
-                                <p class="name">Web Dev group</p>
-                                <p class="message">Does anyone here knows how...</p>
+                                <p className="name">Web Dev group</p>
+                                <p className="message">Does anyone here knows how...</p>
                             </td>
-                            <td class="timestamp">11:52am</td>
+                            <td className="timestamp">11:52am</td>
                         </tr>
                         <tr data-image="./images-inbox/msg4.png">
                             <td>
-                                <p class="name">Photo shoot group</p>
-                                <p class="message">Anyone free to...</p>
+                                <p className="name">Photo shoot group</p>
+                                <p className="message">Anyone free to...</p>
                             </td>
-                            <td class="timestamp">2:10pm</td>
+                            <td className="timestamp">2:10pm</td>
                         </tr>
                         <tr data-image="./images-inbox/msg5.png">
                             <td>
-                                <p class="name">Lee Cheng</p>
-                                <p class="message">Hi, I will be assigning...</p>
+                                <p className="name">Lee Cheng</p>
+                                <p className="message">Hi, I will be assigning...</p>
                             </td>
-                            <td class="timestamp">5:48pm</td>
+                            <td className="timestamp">5:48pm</td>
                         </tr>
                     </table>
                 </div>
             </div>
 
-            <div class="line"></div>
+            <div className="line"></div>
 
-            <div class="column-message">
-                <div class="default-display" id="default-display">
+            <div className="column-message">
+                <div className="default-display" id="default-display">
                 </div>
                 <div>
                     <img id="image-container" src="/images-inbox/msg1.png" alt="Message container" />
                 </div>
 
 
-                <div class="message-box" id="message-box">
-                    <textarea class="message-input" id="message-input" placeholder="Type your message"></textarea>
-                    <img class="send-vector" id="send-vector" src="./images-inbox/send-vector.png" alt="Send" />
+                <div className="message-box" id="message-box">
+                    <textarea className="message-input" id="message-input" placeholder="Type your message"></textarea>
+                    <img className="send-vector" id="send-vector" src="./images-inbox/send-vector.png" alt="Send" />
                 </div>
             </div>
 
         </div>
-    )
-}
+    );
+};
 
-export default Inbox
+export default Inbox;
--- flexjobs-client/src/modules/Module3/feedback.jsx
+++ flexjobs-client/src/modules/Module3/feedback.jsx
@@ -4,45 +4,49 @@ import './script-feedback.js';
 
 const Feedback = () => {
     return (
-        <div class="container-feedback">
-            <form id="feedback-form">
-                <h1>Feedback Form</h1>
-                <div class="id">
-                    <input type="text" placeholder="Name" required/>
-                        <i class="far fa-user profile-icon"></i>
-                </div>
-                <div class="id">
-                    <input type="email" placeholder="Email address" required/>
-                        <i class="far fa-envelope email-icon"></i>
-                </div>
+        <div className="section-feedback">
+            <section></section>
+            <div className="container-feedback">
+                <form id="feedback-form">
+                    <h1>Feedback Form</h1>
+                    <div className="id">
+                        <input type="text" placeholder="Name" required />
+                        <i className="far fa-user profile-icon"></i>
+                    </div>
+                    <div className="id">
+                        <input type="email" placeholder="Email address" required />
+                        <i className="far fa-envelope email-icon"></i>
+                    </div>
 
-                <label for="category" class="category">Category: </label>
-                <select id="category" type="dropdown" size="1" required>
-                    <option></option>
-                    <option>Website UI</option>
-                    <option>Home</option>
-                    <option>Jobs</option>
-                    <option>Chats</option>
-                    <option>Career Test Assessment</option>
-                    <option>Payment</option>
-                    <option>Others</option>
-                </select>
+                    <label for="category" className="category">Category: </label>
+                    <select id="category" type="dropdown" size="1" required>
+                        <option></option>
+                        <option>Website UI</option>
+                        <option>Home</option>
+                        <option>Jobs</option>
+                        <option>Chats</option>
+                        <option>Career Test Assessment</option>
+                        <option>Payment</option>
+                        <option>Others</option>
+                    </select>
 
 
-                <div class="rating">
-                    <label for="rating-text" class="rating-text">Rating: </label>
-                    <span class="star" data-value="1">&#9733;</span>
-                    <span class="star" data-value="2">&#9733;</span>
-                    <span class="star" data-value="3">&#9733;</span>
-                    <span class="star" data-value="4">&#9733;</span>
-                    <span class="star" data-value="5">&#9733;</span>
-                </div>
+                    <div className="rating">
+                        <label for="rating-text" className="rating-text">Rating: </label>
+                        <span className="star" data-value="1">&#9733;</span>
+                        <span className="star" data-value="2">&#9733;</span>
+                        <span className="star" data-value="3">&#9733;</span>
+                        <span className="star" data-value="4">&#9733;</span>
+                        <span className="star" data-value="5">&#9733;</span>
+                    </div>
 
-                <textarea cols="15" rows="5" placeholder="Enter your feedback" required></textarea>
-                <button id="submitButton" type="submit">Send</button>
-            </form>
+                    <textarea cols="15" rows="5" placeholder="Enter your feedback" required></textarea>
+                    <button id="submitButton" type="submit">Send</button>
+                </form>
+            </div>
         </div>
-    )
-}
 
-export default Feedback
+    );
+};
+
+export default Feedback;
