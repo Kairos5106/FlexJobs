b2b42d7fa217ce7f50cdb77fd2f75dc5fa1e5170
kevinhotzewan <kevinhotzewan@gmail.com>
2024-06-07 20:38:44 +0800

    added some code for backend from Aireen

--- flexjobs-server/index.js
+++ flexjobs-server/index.js
@@ -31,6 +31,7 @@ async function run() {
     const database = client.db("flexjobs-database");
     const users = database.collection("users");
 
+    /*
     // Create a document to insert
     const personOne = {
         username: "sample_username",
@@ -42,7 +43,101 @@ async function run() {
 
     // Print the ID of the inserted document
     console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    */
 
+    // Module 2 ---------------------------------------------------------------------------------------------------------
+    const jobsCollection = database.collection("jobs");
+    
+    // Post a job
+    app.post("/post-job", async (req, res) => {
+      const body = req.body;
+      body.createdAt = new Date();
+      try {
+        const insertJob = await jobsCollection.insertOne(body);
+        if (insertJob.insertedId) {
+          return res.status(200).send(insertJob);
+        } else {
+          return res.status(404).send({
+            message: "Cannot insert. Try again later.",
+            status: false
+          });
+        }
+      } catch (error) {
+        return res.status(500).send({
+          message: "Internal Server Error",
+          status: false
+        });
+      }
+    });
+
+    // Get all jobs
+    app.get("/all-jobs", async (req, res) => {
+      try {
+        const jobs = await jobsCollection.find({}).toArray();
+        res.send(jobs);
+      } catch (error) {
+        res.status(500).send({
+          message: "Internal Server Error",
+          status: false
+        });
+      }
+    });
+
+    // Get a single job using id (to be displayed in JobDetails.jsx)
+    const { ObjectId } = require('mongodb');
+
+    app.get("/all-jobs/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const job = await jobsCollection.findOne({
+          _id: new ObjectId(id)
+        });
+        res.send(job);
+      } catch (error) {
+        res.status(500).send({
+          message: "Internal Server Error",
+          status: false
+        });
+      }
+    });
+
+    // Apply for job
+    const jobApplicationsCollection = database.collection("jobApplications");
+    app.post("/post-job-application", async (req, res) => {
+      const body = req.body;
+      body.createdAt = new Date();
+      try {
+        const insertApplication = await jobApplicationsCollection.insertOne(body);
+        if (insertApplication.insertedId) {
+          return res.status(200).send(insertApplication);
+        } else {
+          return res.status(404).send({
+            message: "Cannot insert. Try again later.",
+            status: false
+          });
+        }
+      } catch (error) {
+        return res.status(500).send({
+          message: "Internal Server Error",
+          status: false
+        });
+      }
+    });
+
+    // Get all job application
+    app.get("/all-job-applications", async (req, res) => {
+      try {
+        const jobApplications = await jobApplicationsCollection.find({}).toArray();
+        res.send(jobApplications);
+      } catch (error) {
+        res.status(500).send({
+          message: "Internal Server Error",
+          status: false
+        });
+      }
+    });
+
+    // Module 2 ---------------------------------------------------------------------------------------------------------
 
 
     
@@ -51,7 +146,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
