f9a35b1ba80ba51fa8e013cf44f3e3f7cc29057d
TheUnknownOriginal <yungzheng90@gmail.com>
2024-06-11 12:04:02 +0800

    fixed styling

--- flexjobs-client/src/modules/Module6/ForumMain.jsx
+++ flexjobs-client/src/modules/Module6/ForumMain.jsx
@@ -54,97 +54,99 @@ const ForumMain = () => {
     }, [searchQuery, popularTopics, featuredTopics]);
 
     return (
-        <div className="forum-header-and-section">
-            {/* Discussion Forum title */}
-            <div className='forum-and-post'>
-                <h1 id="discussion-forum-title">Discussion Forum</h1>
-                <Link to="/NewForum" className="add-post-button">Add New Topic</Link>
-            </div>
-            
-            {/* Search bar and add new post button */}
-            <div className="search-bar">
-                <input
-                    type="text"
-                    id="search-input"
-                    placeholder="What are you looking for?"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-            </div>
-
-
-           {/* Search results dropdown */}
-    {searchResults.length > 0 && (
-        <div className="search-results-dropdown">
-            {searchResults.map(topic => (
-                <div className="searched-item" key={topic._id}>
-                    <Link to={`/forum/${topic._id}`} className="topics" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <div>
-                            <h1 className='searched-title'>{topic.title}</h1>
-                        </div>
-                        <div>
-                            <p className='searched-date'>{new Date(topic.timestamp).toLocaleDateString()}</p>
-                        </div>
-                    </Link>
+        <div id='forum' className=' tw-my-[-150px]'>
+            <div className="forum-header-and-section">
+                {/* Discussion Forum title */}
+                <div className='forum-and-post'>
+                    <h1 id="discussion-forum-title">Discussion Forum</h1>
+                    <Link to="/NewForum" className="add-post-button">Add New Topic</Link>
                 </div>
-            ))}
-        </div>
-    )}
+                
+                {/* Search bar and add new post button */}
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        id="search-input"
+                        placeholder="What are you looking for?"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                </div>
 
 
-            {/* Current popular topics */}
-            <h1 className="topics-title-popular">Current Popular Topics</h1>
-
-            <div className="recycler-view-popular-topics">
-                {/* Display popular topics */}
-                {popularTopics.map(topic => (
-                    <div className="topics-item" key={topic._id}>
-                        <div className="left">
-                            <Link to={`/forum/${topic._id}`} className="topics">
-                                <h1>{topic.title}</h1>
-                            </Link>
-                        </div>
-                        <div className="middle">
-                            <h3 className="latestby">By</h3>
-                            <h1 className="latestauthor">{topic.username ? topic.username : 'Anonymous'}</h1>
-                        </div>
-                        <div className="right">
-                            {/* Convert the timestamp to a human-readable format */}
-                            <h1 className="latesttime">{new Date(topic.timestamp).toLocaleDateString()}</h1>
-                        </div>
+            {/* Search results dropdown */}
+        {searchResults.length > 0 && (
+            <div className="search-results-dropdown">
+                {searchResults.map(topic => (
+                    <div className="searched-item" key={topic._id}>
+                        <Link to={`/forum/${topic._id}`} className="topics" style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <div>
+                                <h1 className='searched-title'>{topic.title}</h1>
+                            </div>
+                            <div>
+                                <p className='searched-date'>{new Date(topic.timestamp).toLocaleDateString()}</p>
+                            </div>
+                        </Link>
                     </div>
                 ))}
             </div>
+        )}
 
-            {/* Featured topics */}
-            <h1 className="topics-title">Featured Topics</h1>
 
-            <div className="recycler-view-popular-topics">
-                {/* Display featured topics */}
-                {featuredTopics.map(topic => (
-                    <div className="topics-item" key={topic._id}>
-                        <div className="left">
-                            <Link to={`/forum/${topic._id}`} className="topics">
-                                <h1>{topic.title}</h1>
-                            </Link>
+                {/* Current popular topics */}
+                <h1 className="topics-title-popular">Current Popular Topics</h1>
+
+                <div className="recycler-view-popular-topics">
+                    {/* Display popular topics */}
+                    {popularTopics.map(topic => (
+                        <div className="topics-item" key={topic._id}>
+                            <div className="left">
+                                <Link to={`/forum/${topic._id}`} className="topics">
+                                    <h1>{topic.title}</h1>
+                                </Link>
+                            </div>
+                            <div className="middle">
+                                <h3 className="latestby">By</h3>
+                                <h1 className="latestauthor">{topic.username ? topic.username : 'Anonymous'}</h1>
+                            </div>
+                            <div className="right">
+                                {/* Convert the timestamp to a human-readable format */}
+                                <h1 className="latesttime">{new Date(topic.timestamp).toLocaleDateString()}</h1>
+                            </div>
                         </div>
-                        <div className="middle">
-                            <h3 className="latestby">By</h3>
-                            <h1 className="latestauthor">{topic.username ? topic.username : 'Anonymous'}</h1>
+                    ))}
+                </div>
+
+                {/* Featured topics */}
+                <h1 className="topics-title">Featured Topics</h1>
+
+                <div className="recycler-view-popular-topics">
+                    {/* Display featured topics */}
+                    {featuredTopics.map(topic => (
+                        <div className="topics-item" key={topic._id}>
+                            <div className="left">
+                                <Link to={`/forum/${topic._id}`} className="topics">
+                                    <h1>{topic.title}</h1>
+                                </Link>
+                            </div>
+                            <div className="middle">
+                                <h3 className="latestby">By</h3>
+                                <h1 className="latestauthor">{topic.username ? topic.username : 'Anonymous'}</h1>
+                            </div>
+                            <div className="right">
+                                
+                                <h1 className="latesttime">{new Date(topic.timestamp).toLocaleDateString()}</h1>
+                            </div>
                         </div>
-                        <div className="right">
-                            
-                            <h1 className="latesttime">{new Date(topic.timestamp).toLocaleDateString()}</h1>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
+
+                {/* Footer */}
+                <footer className="footer">
+                    <p>&copy; 2024 by Team 20</p>
+                    <p><a href="mailto:u2100667@siswa.um.edu.my">u2100667@siswa.um.edu.my</a></p>
+                </footer>
             </div>
-
-            {/* Footer */}
-            <footer className="footer">
-                <p>&copy; 2024 by Team 20</p>
-                <p><a href="mailto:u2100667@siswa.um.edu.my">u2100667@siswa.um.edu.my</a></p>
-            </footer>
         </div>
     );
 };
