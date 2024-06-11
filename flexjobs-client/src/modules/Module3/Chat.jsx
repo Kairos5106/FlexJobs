56c4ab81bda98b9308ea801386c7a4c6647c9862
dominicchong <rydominicchong@gmail.com>
2024-06-11 17:32:56 +0800

    Chat remove comments

--- flexjobs-client/src/modules/Module3/Chat.jsx
+++ flexjobs-client/src/modules/Module3/Chat.jsx
@@ -2,21 +2,13 @@ import React, { useState } from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './style-chat.css';
 import ChatFeed from './component/ChatFeed';
-// import LoginForm from'./LoginForm';
-// import DefaultChat from './DefaultChat';
-// import { UserContext } from '../../context/userContext';
 
 const Chat = () => {
-    // if(!localStorage.getItem('username')) return <LoginForm />
-    // if(!localStorage.getItem('username')) return <DefaultChat />
-    // const { user } = UserContext(UserContext); 
 
     return (
         <ChatEngine
             height="100vh"
             projectID="1b440d10-9c1b-431e-8bfc-e026350e8c6e"
-            // userName={localStorage.getItem('username')}
-            // userSecret={localStorage.getItem('password')}
             userName='john_doe'
             userSecret='123456'
             renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps} />}
