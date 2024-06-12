e6c3ff23c307bcc344f65926d7b12e3b6095d964
dominicchong <rydominicchong@gmail.com>
2024-06-12 13:29:28 +0800

    Chat projectID test

--- flexjobs-client/src/modules/Module3/Chat.jsx
+++ flexjobs-client/src/modules/Module3/Chat.jsx
@@ -8,7 +8,7 @@ const Chat = () => {
     return (
         <ChatEngine
             height="100vh"
-            projectID="1b440d10-9c1b-431e-8bfc-e026350e8c6e"
+            // projectID="1b440d10-9c1b-431e-8bfc-e026350e8c6e"
             userName='john_doe'
             userSecret='123456'
             renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps} />}
